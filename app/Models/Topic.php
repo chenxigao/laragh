4ea40a52b1a552bc55e52fb5ab5761a0f65a6bf0
vagrant <vagrant@homestead>
2019-08-02 10:00:35 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,22 @@
 
 namespace App\Models;
 
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        //一个话题只能属于 一个分类
+        return $this->belongsTo(Category::class);
+    }
+
+
+    public function user()
+    {
+        //一个话题拥有一个作者
+        return $this->belongsTo(User::class);
+    }
+
 }
