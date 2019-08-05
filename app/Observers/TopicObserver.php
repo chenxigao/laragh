5732e9a70a4b7385c3e3f1fae92c41fb221b515c
vagrant <vagrant@homestead>
2019-08-05 11:31:41 +0000

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,9 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        //对话题内容进行过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
