68c2464a4a97a5f23374f5fd0a706fa37a54e0cf
vagrant <vagrant@homestead>
2019-08-05 11:38:43 +0000

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -39,9 +39,10 @@
 
                                     <div class="form-group">
                                         <select class="form-control" name="category_id" required/>
-                                        <option value="" hidden disabled selected>请选择分类</option>
+                                        <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类
+                                        </option>
                                         @foreach($categories as $value)
-                                            <option value="{{ $value->id }}">{{ $value->name }}</option>
+                                            <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
                                             @endforeach
                                             </select>
                                     </div>
@@ -81,8 +82,8 @@
             var editor = new Simditor({
                 textarea: $('#editor'),
                 upload: {
-                    url:'{{ route('topics.upload_image') }}',
-                    params:{
+                    url: '{{ route('topics.upload_image') }}',
+                    params: {
                         _token: '{{ csrf_token() }}'
                     },
                     fileKey: 'upload_file',
@@ -90,7 +91,7 @@
                     leaveConfirm: '文件上传中，关闭此页面将取消上传！',
                 },
 
-                pasteImage:true,
+                pasteImage: true,
 
             });
         });
