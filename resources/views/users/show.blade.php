022ae536c781ce1a4ced5904a56667f6c9be3ffb
vagrant <vagrant@homestead>
2019-08-13 10:17:30 +0000

    回复列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
             <div class="card">
                 <img class="card-img-top" src="{{ $user->avatar }}"
-                alt="{{ $user->name }}">
+                     alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
                     <p>{{ $user->introduction }}</p>
@@ -14,12 +14,14 @@
                     <h5><strong>注册于</strong></h5>
                     <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
-                </div>
             </div>
+        </div>
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
             <div class="card">
                 <div class="card-body">
-                    <h1 class="mb-0" style="font-size: 22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+                    <h1 class="mb-0" style="font-size: 22px;">{{ $user->name }}
+                        <small>{{ $user->email }}</small>
+                    </h1>
                 </div>
             </div>
             <hr>
@@ -28,10 +30,19 @@
             <div class="card">
                 <div class="card-body">
                     <ul class="nav  nav-tabs">
-                        <li class="nav-item"><a class="nav-link active bg-transparent"  href="#">Ta 的话题</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="">Ta 的回复</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}"
+                               href="{{ route('users.show', $user->id) }}">Ta 的话题</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}"
+                               href="{{ route('users.show', [ $user->id, 'tab' => 'replies']) }}">Ta 的回复</a></li>
                     </ul>
-                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @if(if_query('tab', 'replies'))
+                        @include('users._replies', ['replies' =>$user->replies()->with('topic')->recent()->paginate(5)])
+                    @else
+                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @endif
                 </div>
             </div>
         </div>
